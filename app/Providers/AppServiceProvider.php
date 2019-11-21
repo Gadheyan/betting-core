2bbc835aa9f8c72944bc5d3deeb83eda1620798c
gadheyan <gadheyan@linways.com>
2019-11-21 13:42:42 +0530

    added social media integration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,23 @@
 
 namespace App\Providers;
 
+use App\Services\SocialUserResolverService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+
+     /**
+     * All of the container bindings that should be registered.
+     *
+     * @var array
+     */
+    public $bindings = [
+        SocialUserResolverInterface::class => SocialUserResolverService::class,
+    ];
+
+
     /**
      * Register any application services.
      *
